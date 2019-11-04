924d25995ab251df7795850e84371f0e6bcecd82
zhangduo <zhangduo@globalegrow.com>
2019-11-04 11:46:55 +0800

    delete code

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import {Button} from 'antd'
 
 export default class App extends React.Component {
     render() {
         return (<>
-            react
+            <Button type="primary">antd button</Button>
         </>)
     }
 }
